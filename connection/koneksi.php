5065ad8fcfbb89d11cd9c7d6ea68aed49d4f00c3
wildanzm <wildanmanapi123@gmail.com>
2025-08-13 07:15:37 +0700

    fix: deploy test

--- connection/koneksi.php
+++ connection/koneksi.php
@@ -5,10 +5,3 @@ $password = '';
 $database_name = 'travelpedia';
 
 $db = mysqli_connect($hostname, $username, $password, $database_name);
-
-// if ($db->connect_error) {
-//    echo 'eror';
-//    die('eror');
-// } else{
-//    echo 'Koneksi Berhasil';
-// }
--- travelpedia.sql
+++ travelpedia.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 10, 2025 at 02:31 PM
+-- Generation Time: Aug 12, 2025 at 11:38 PM
 -- Server version: 8.0.30
--- PHP Version: 8.1.31
+-- PHP Version: 8.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,18 +41,6 @@ CREATE TABLE `pemesanan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `pemesanan`
---
-
-INSERT INTO `pemesanan` (`id`, `nama`, `no_hp`, `waktu_wisata`, `hari_wisata`, `paket_wisata`, `jumlah_peserta`, `total_tagihan`, `is_deleted`, `created_at`) VALUES
-(1, 'Agus', '0812345645', '2025-01-16', 2, 'Paket Silver', 1, 2500000, 0, '2025-01-04 07:09:55'),
-(2, 'Aceng', '08342343', '2025-01-22', 3, 'Paket Gold', 1, 4950000, 0, '2025-01-04 07:12:26'),
-(3, 'Asep', '08343243246', '2025-02-06', 2, 'Paket Silver', 4, 10000000, 1, '2025-01-08 02:02:03'),
-(4, 'Asep', '084323423', '2025-01-30', 2, 'Paket Silver', 2, 5000000, 0, '2025-01-08 02:02:40'),
-(5, 'Andi', '083123212414', '2025-02-20', 4, 'Paket Bronze', 2, 6000000, 0, '2025-01-08 02:03:17'),
-(6, 'Anton', '081232373', '2025-03-06', 2, 'Paket Gold', 2, 6600000, 0, '2025-01-08 02:03:48');
-
---
 -- Indexes for dumped tables
 --
 
@@ -70,7 +58,7 @@ ALTER TABLE `pemesanan`
 -- AUTO_INCREMENT for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
